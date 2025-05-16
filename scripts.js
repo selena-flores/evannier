db0627e309f56547045cec5c60c73116c548a0d4
selenavonichor <selenavonichor@gmail.com>
2025-05-15 21:12:27 -0700

    new format

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 $(window).on("load", function () {
   $("button").click(function () {
+    $("#" + this.id).toggleClass("active");
     $("#" + this.id + "_content").toggle(this.id);
     $('html, body').animate({scrollTop: '0px'}, 100);
   });
